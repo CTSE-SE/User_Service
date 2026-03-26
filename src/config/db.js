295925a8c3133149e044bb4476cb2dd2119dbd9c
Shani Ishara <140489217+shaniranasinghe@users.noreply.github.com>
2026-03-26 10:51:35 +0530

    update db.js
    update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,7 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD loaded:", typeof process.env.DB_PASSWORD);
+const useSSL = process.env.DB_SSL === "true";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -13,7 +11,15 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     dialect: "postgres",
-    logging: false
+    logging: false,
+    dialectOptions: useSSL
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
   }
 );
 
